920ff471411a600ace9d12119d7a9ab2022f45c0
Anigko <anigko@yandex.ru>
2022-03-06 18:15:01 +0300

    Initial commit

---
+++ src/ru/netology/ak1/Calculator.java
@@ -0,0 +1,29 @@
+package ru.netology.ak1;
+
+import java.util.function.*;
+
+public class Calculator {
+
+    static Supplier<Calculator> instance = Calculator::new;
+
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    BinaryOperator<Integer> devide = (x, y) -> y == 0 ? devisionByZero(x, y) : x / y;
+
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+
+    Predicate<Integer> isPositive = x -> x > 0;
+
+    Consumer<Integer> println = System.out::println;
+
+    private Integer devisionByZero(Integer x, Integer y) {
+        try {
+            int value = x / y;
+        } catch (ArithmeticException e) {
+            System.out.println("Арифметическая ошибка");
+        }
+        return 0;
+    }
+}
